7e00c2f1b7ee7ccd8d7bb0d0222954cd6a65f8e9
easter0731 <easter0731@gmail.com>
2014-12-04 16:40:20 -0500

    2

--- Bioconductor.R
+++ Bioconductor.R
@@ -3,10 +3,11 @@ installpkg <- function(pkg){
         if(!require(pkg,character.only=T)){
                 source("http://bioconductor.org/biocLite.R")
                 biocLite(pkg)
-        }else{
-                require(pkg,character.only=T)
         }
+        require(pkg,character.only=T)
 }
 
 installpkg('seqinr')
-?require
+
+library("ggplot2")
+choosebank("genbank")
